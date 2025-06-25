d40d3d602b2fe54cc479813e63ae0f847db0e8d8
waquar ahmed <waquarahmednawaz@gmail.com>
2025-06-25 17:58:16 +0530

    ♻️ env setup for frontend URL and cleaned socket.io cors config

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   withCredentials: true,
 });
 
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ const onlineUsers = new Map(); // socketId : userId
 export const setupSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     },
   });
