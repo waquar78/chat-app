fd18f2f6a4b939cc72b17c80b272fcde02276877
waquar78 <141168348+waquar78@users.noreply.github.com>
2025-09-05 00:26:21 +0530

    Update generateToken.js
    "fix: resolved merge conflict in generateToken.js"

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,20 +1,23 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const generateToken= (res,user,message,)=>{
-  const token=jwt.sign({userId:user._id},process.env.SECRET_KEY, {expiresIn: "1d"})
-  return res.status(200).cookie("token",token,{
-    httpOnly: true,
-    sameSite: "None",
-<<<<<<< HEAD
-    secure:true,
-=======
-     secure: true, 
->>>>>>> e8a9ebda1cacfa69e9f184307c67fe6d0097cdec
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-}).json({
-    success:true,
-    message,
-    user
-})
+export const generateToken = (res, user, message) => {
+  const token = jwt.sign(
+    { userId: user._id },
+    process.env.SECRET_KEY,
+    { expiresIn: "1d" }
+  );
 
-}
+  return res
+    .status(200)
+    .cookie("token", token, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    })
+    .json({
+      success: true,
+      message,
+      user,
+    });
+};
